c9d699d61714bc1b084f92349fb7521d9edf76d5
Mike Morse <62817947+mmorsecode@users.noreply.github.com>
2026-08-19 13:24:38 -0400

    screen is working

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+import constants
+from logger import log_state
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+            return
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
