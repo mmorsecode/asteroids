b188db28a0dc06dda22b2914190f035a4b13d474
Mike Morse <62817947+mmorsecode@users.noreply.github.com>
2026-08-19 13:38:06 -0400

    added delta time

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt: float = 0.0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
